abddd4113da0bf61ca818b77e08a8e1712045749
siushen <784843867@qq.com>
2016-08-05 18:05:45 +0800

    add Version

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Bitmap Scale
 ---------
 Screen
 ---------
+Version
+--------
---
+++ utilsbag/src/main/java/com/xs/utilsbag/app/VersionUtil.java
@@ -0,0 +1,50 @@
+package com.xs.utilsbag.app;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * @version V1.0 <app版本号获取>
+ * @author: Xs
+ * @date: 2016-08-05 17:29
+ * @email Xs.lin@foxmail.com
+ */
+public class VersionUtil {
+
+    /**
+     * 获取版本号
+     *
+     * @param context 上下文
+     * @return 版本号
+     */
+    public static String getVersionName(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        String packageName = context.getPackageName();
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "1.0.0";
+    }
+
+    /**
+     * 获取版本code
+     *
+     * @param context 上下文
+     * @return 版本code
+     */
+    public static int getVersionCode(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        String packageName = context.getPackageName();
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+}
